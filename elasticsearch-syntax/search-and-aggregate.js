fefb257b2ea0feb06804caa5ce26ce02d5b86dfa
James Wachmann <james.wachmann@gmail.com>
2022-05-17 23:29:01 -0400

    Added searching by a field and sorting by a field

--- elasticsearch-syntax/search-and-aggregate.js
+++ elasticsearch-syntax/search-and-aggregate.js
@@ -1,7 +1,17 @@
 // Search queries are directed to the _search api
 // GET people/_search
 
-
+// Get docs that match the given field, return the top 5 results
+// GET people/_search
+q=
+{
+  "query": {
+    "match": {
+      "job": "Engineer"
+    }
+  },
+  "size": 5
+}
 
 // Get list of values for a field and how many docs contain it (categories)
 // GET people/_search
@@ -15,4 +25,17 @@ q=
       }
     }
   }
+}
+
+// Sort documents by a field
+//GET people/_search
+q=
+{
+  "sort": [
+    {
+      "name": {
+        "order": "desc"
+      }
+    }
+  ]
 }
